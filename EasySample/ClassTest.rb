d32b3fc7fccca431fdff3a6f192283fb1ec297ea
squallcloud <squallcloudzidane@gmail.com>
2018-10-07 00:04:24 +0900

    merge: workbranchをマージ

---
+++ EasySample/ClassTest.rb
@@ -0,0 +1,20 @@
+﻿class Hoge
+  def initialize()
+    @name = "ほげ"
+  end
+  def initialize(_name)
+    @name = _name
+  end
+  attr_accessor :name
+
+  def Exec()
+    puts("Hoge::Exec()")
+  end
+end
+
+# hoge = Hoge.new()
+# hoge.Exec()
+# puts(hoge.name)
+
+foo = Hoge.new("ホゲ")
+puts(foo.name)
---
+++ EasySample/ExecProcess.rb
@@ -0,0 +1,13 @@
+﻿# system('dir')
+
+require 'open3'
+
+res = Open3.capture3("dir")
+puts res.size()
+idx = 0
+res.each { |it|
+  # puts it.class
+  puts "#{idx} : #{it}"
+  idx += 1
+}
+# puts res.class
