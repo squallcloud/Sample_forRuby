6c2e93fcd70a9da0571f3820bc4ef343e554c6da
squallcloud <squallcloudzidane@gmail.com>
2018-10-08 22:47:22 +0900

    merge: workbranchをマージ

--- EasySample/ClassTest.rb
+++ EasySample/ClassTest.rb
@@ -1,20 +1,53 @@
 ﻿class Hoge
-  def initialize()
-    @name = "ほげ"
-  end
-  def initialize(_name)
+  TEISUU = 100
+  @@fullName = ""
+  def initialize(_name = "ほげ")
     @name = _name
+    @@fullName = _name
   end
   attr_accessor :name
+  attr_accessor :fullName
+
+  def getFullName()
+    return @@fullName
+  end
 
   def Exec()
     puts("Hoge::Exec()")
   end
+
+  def ExecArgs(a, b, c)
+    # @つけなくても変数はつかえるみたい
+    puts("Hoge::ExecArgs() #{@name}")
+  end
 end
 
-# hoge = Hoge.new()
-# hoge.Exec()
-# puts(hoge.name)
+class Foo < Hoge
+  def initialize(_name = "ふー")
+    super
+    # @name = _name
+  end
 
-foo = Hoge.new("ホゲ")
-puts(foo.name)
+  def Exec()
+    puts("Foo::Exec()")
+    super
+  end
+end
+
+hoge = Hoge.new
+puts hoge.name
+hoge.Exec
+hoge.ExecArgs 1, 2, 3
+
+puts "フルネーム" + hoge.getFullName()
+
+foo = Foo.new "ふーー"
+puts foo.name
+foo.Exec
+foo.ExecArgs 1, 2, 3
+
+puts "フルネーム" + hoge.getFullName()
+puts "フルネーム" + foo.getFullName()
+
+# Hoge::TEISUU = 20 #上書きできてしまう。ただWarningは出してくれる。
+puts "定数" + "#{Hoge::TEISUU}"
--- EasySample/ExecProcess.rb
+++ EasySample/ExecProcess.rb
@@ -1,13 +1,22 @@
 ﻿# system('dir')
 
 require 'open3'
+require 'systemu'
 
-res = Open3.capture3("dir")
-puts res.size()
-idx = 0
-res.each { |it|
-  # puts it.class
-  puts "#{idx} : #{it}"
-  idx += 1
-}
+# res = Open3.capture3("dir")
+# idx = 0
+# res.each { |it|
+#   puts "#{idx} : #{it}"
+#   idx += 1
+# }
 # puts res.class
+
+o, e, s = Open3.capture3("dir /?")
+
+print(o + "\n")
+print(e + "\n")
+print("#{s.class}" + "\n")
+
+# cmd = 'dir'
+# status, stdout, stderr = systemu(cmd)
+# print([status, stdout, stderr])
